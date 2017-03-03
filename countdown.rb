176593f2a16a494ce7c8147e0da5f739469fe1f5
Curtis Greene <curtis.j.w.greene@gmail.com>
2017-03-03 20:40:42 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(number)
+ counter = number
+ while counter > 0
+   puts "#{counter} SECOND(S)!"
+   counter -= 1 #we are counting down here folks
+ end
+  "HAPPY NEW YEAR!" #returns the last line of code, rather than printing it
 end
+
+def countdown_with_sleep(number)
+  counter = number
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    sleep(1) #puts program to sleep for amount of seconds
+  end
+   "HAPPY NEW YEAR!"
+ end
